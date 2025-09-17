510ed62724475369b7624dce1542e44bf4ea5050
hyunho <kimhyunhi1@gmail.com>
2025-09-18 00:26:57 +0900

    add: sp

--- arduino-nano-p/src/main.cpp
+++ arduino-nano-p/src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
 
-#define PIEZO_PIN 8     // 피에조 부저 핀
-#define BUTTON_PIN 2    // 버튼 핀 (디지털 핀 2번 사용)
+#define SPEAKER_PIN 8   // 스피커 핀
+#define BUTTON_PIN 2    // 버튼 핀
 
 void setup() {
-  pinMode(PIEZO_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP); // 버튼을 풀업 입력으로 설정 (눌리면 LOW)
+  pinMode(SPEAKER_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP); // 풀업 버튼 (눌리면 LOW)
 }
 
 void loop() {
-  if (digitalRead(BUTTON_PIN) == LOW) {   // 버튼 눌림 감지
-    tone(PIEZO_PIN, 1000, 400); // 400ms 동안 울림
-    delay(450);
-    tone(PIEZO_PIN, 1200, 400);
-    delay(450);
+  if (digitalRead(BUTTON_PIN) == LOW) { // 버튼 눌렸을 때
+    tone(SPEAKER_PIN, 2000, 150);  // 2000Hz 150ms
+    delay(10);
+    noTone(SPEAKER_PIN);           // 소리 끔
   }
 }
