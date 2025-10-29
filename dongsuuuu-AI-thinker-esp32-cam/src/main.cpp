70dc2aa8f39c36ba1d28ac000af6471528c365b2
hyunho <kimhyunhi1@gmail.com>
2025-10-29 09:38:03 +0900

    add: class & 25000정도는 되야할듯

--- dongsuuuu-AI-thinker-esp32-cam/src/main.cpp
+++ dongsuuuu-AI-thinker-esp32-cam/src/main.cpp
@@ -1,14 +1,50 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include <esp_now.h>
 
 #define LED_PIN1 32
 #define LED_PIN2 33
 #define BUTTON_PIN 14
 
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+//uint8_t broadcastAddress[] = {0x88, 0x85, 0x27, 0x11, 0x55, 0x45};
+
+typedef struct struct_message {
+    char a[32];
+    int b;
+    float c;
+    bool d;
+} struct_message;
+
+struct_message myData;
+
+esp_now_peer_info_t peerInfo; //데이터 송수신 할 그룹 지정할때 필요
+
 // Declare task handle
 TaskHandle_t Task1Handle = NULL;
 TaskHandle_t Task2Handle = NULL;
-TaskHandle_t TaskButtonHandle = NULL;
+TaskHandle_t TaskButtonHandle= NULL;
+uint8_t baseMac[6];
 
+void readMacAddress() {
+  uint8_t mac[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+  if(ret == ESP_OK) {
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                baseMac[0], baseMac[1], baseMac[2],
+                baseMac[3], baseMac[4], baseMac[5]);
+  } else {
+    Serial.println("Failed to get MAC address");
+  };
+}
+
+void onDataRecv(const uint8_t * mac, const uint8_t * incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("Char : ");
+}
 void Task1(void *parameter) {
   for (;;) { // Infinite loop
     digitalWrite(LED_PIN1, HIGH);
@@ -23,6 +59,20 @@ void Task1(void *parameter) {
     Serial.println(xPortGetCoreID());
 
     Serial.printf("Task1 Stack Free: %u bytes\n", uxTaskGetStackHighWaterMark(NULL));
+
+    // ESP-NOW 통신
+    strcpy(myData.a, "ESP-NOW TEST SEND");
+    myData.b = random(0, 100);
+    myData.c = 1.2;
+    myData.d = false;
+
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+
+    if(result == ESP_OK) {
+      Serial.println("Sent with success");
+    } else {
+      Serial.println("Error sending the data");
+    }
   }
 }
 
@@ -42,28 +92,46 @@ void Task2(void *parameter) {
     Serial.printf("Task2 Stack Free: %u bytes\n", uxTaskGetStackHighWaterMark(NULL));
   }
 }
-
-void TaskButton(void *paramter) {
-  for (;;) { // Infinite loop
-    // digitalRead(BUTTON_PIN)
-    버튼 인식하면
-      vTaskSuspend(Task1Handle);
-
-    버튼 인식하면
-      vTaskResume(Task1Handle);
+void TaskButton(void *parameter) {
+  for (;;){
+  if(digitalRead(BUTTON_PIN) == HIGH){
+  vTaskSuspend(Task1Handle);
   }
+  if(digitalRead(BUTTON_PIN) == HIGH){
+  vTaskResume(Task1Handle);
 }
-
+}
+}
 void setup() {
   Serial.begin(115200);
 
   delay(1000);
   Serial.printf("Starting FreeRTOS: Memory Usage\nInitial Free Heap: %u bytes\n", xPortGetFreeHeapSize());
- 
+  WiFi.mode(WIFI_STA);
+  WiFi.begin();
+
+  Serial.println("ESP32 Board MAC Address: ");
+  readMacAddress();
+//1 init esp-now
+  if(esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+//2 register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+//3 add peer
+  if(esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+//4 recive callback
+  esp_now_register_recv_cb(esp_now_recv_cb_t(onDataRecv));
+
   pinMode(LED_PIN1, OUTPUT);
   pinMode(LED_PIN2, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT);
-
+  pinMode(BUTTON_PIN, OUTPUT);
   xTaskCreatePinnedToCore(
     Task1,         // Task function
     "Task1",       // Task name
@@ -90,5 +158,7 @@ void loop() {
   if (millis() - lastCheck > 5000) {
     Serial.printf("Free Heap: %u bytes\n", xPortGetFreeHeapSize());
     lastCheck = millis();
+
+
   }
 }
